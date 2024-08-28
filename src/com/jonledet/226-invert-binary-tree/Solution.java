7ba51871da3df6f813283532c5e85b64b2d99403
jonledet <jonledet@outlook.com>
2024-08-28 11:43:42 -0500

    Package structure

---
+++ src/com/jonledet/226-invert-binary-tree/Solution.java
@@ -0,0 +1,29 @@
+package com.jonledet.226-invert-binary-tree;
+
+import com.jonledet.types.TreeNode;
+
+/*
+ * Runtime:
+ * 0 ms
+ * Beats 100.00%
+ * 
+ * Memory:
+ * 41.08 MB
+ * Beats 47.13%
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+
+        return root;
+    }
+}
